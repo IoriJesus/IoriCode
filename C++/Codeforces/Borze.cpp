56ec1f8410b995133569b57178b4c9c3cfc95944
IoriJesus <57327567+IoriJesus@users.noreply.github.com>
2020-07-19 01:09:29 -0500

    Update Borze.cpp

--- C++/Codeforces/Borze.cpp
+++ C++/Codeforces/Borze.cpp
@@ -4,8 +4,8 @@
  
 using namespace std;
  
-int main()
-{
+int main(){
+ 
     vector<int> valores{};
     string mensajeclave;
     
@@ -13,24 +13,20 @@ int main()
     
     for(int i=0; i<mensajeclave.size();i++){
     
-    if(mensajeclave[i] == '.' )
-    valores.push_back(0); 
-    
-    if(mensajeclave[i] == '-' and mensajeclave[i+1] == '.'){
-       valores.push_back(1);  
-       i++;
+      if(mensajeclave[i] == '.' ) valores.push_back(0); 
+
+      if(mensajeclave[i] == '-' and mensajeclave[i+1] == '.'){
+         valores.push_back(1);  
+         i++;
+      }
+
+      if(mensajeclave[i] == '-' and mensajeclave[i+1] == '-'){
+         valores.push_back(2); 
+         i++;
+      }
     }
     
-    if(mensajeclave[i] == '-' and mensajeclave[i+1] == '-'){
-       valores.push_back(2); 
-       i++;
-    }
-    
-    }
-    
-    for(int i=0; i<valores.size();i++){
-        cout<<valores[i];
-    }
+    for(int i=0; i<valores.size();i++) cout<<valores[i];
     
     return 0;
 }
